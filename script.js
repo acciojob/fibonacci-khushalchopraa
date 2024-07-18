57466160c4d8b1e17427912855e8ca3dbe27f89a
khushalchopraa <advkhushal97@gmail.com>
2024-07-18 11:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	 if (num <= 0) {
+        return "Input should be a positive integer";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let a = 0, b = 1;
+        for (let i = 2; i < num; i++) {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
 }
 
 module.exports = fibonacci;
